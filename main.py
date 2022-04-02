8d33086cf45ae8c80950f68853cfadf2fdeb560c
FranZajara <franzajaragomez@gmail.com>
2022-04-02 13:23:45 +0200

    actual

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, Request
 import json
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -14,5 +15,13 @@ async def calc(request:Request):
     return num1*num2
     
 
+class Post(BaseModel):
+    C1: int
+    C2: int
+    C3: int
+    C4: int
 
-
+@app.post('/posts')
+def save_post(post: Post):
+    print (post)
+    return 'recibido'
