e87612b32661a2a1c0bc7a189ac57805c5c9e6a5
FranZajara <franzajaragomez@gmail.com>
2022-04-08 16:18:35 +0200

    jinja

--- main.py
+++ main.py
@@ -10,29 +10,6 @@ app = FastAPI()
 
 templates = Jinja2Templates(directory="templates")
 
-@app.get('/inicio')
-async def rutadeprueba():
-    return "Hola desde FastAPI"
-
-@app.get("/mul")
-async def calc(request:Request):
-    num1= int(request.form.get('num1'))
-    num2= int(request.form.get('num2'))
-    return num1*num2
-    
-class Respuestas(BaseModel):
-    number1: int
-    number: int
-
-    class Config:
-        orm_mode = True
-
-@app.post('/posts')
-async def create(respuesta: Respuestas):
-    return respuesta
-    
-    #return Pica(respuesta[0], respuestaj[1])
-
 @app.post("/analisis/", response_class=HTMLResponse)
 async def analisis(request = Request, nombre: str = Form(...), 
                 edad: int = Form(...), 
