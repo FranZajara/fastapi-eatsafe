6169b949252444e3c187377e5d5e7309964ca7bc
FranZajara <franzajaragomez@gmail.com>
2022-04-08 15:38:24 +0200

    jinja

--- main.py
+++ main.py
@@ -55,5 +55,25 @@ async def analisis(request = Request, nombre: str = Form(...),
                 cuestion16: int = Form(...)):
     
     
-    return templates.TemplateResponse("analisis.html", {"request" : request, "cuestion1" : cuestion1})
+    return templates.TemplateResponse("analisis.html", {"request" : request, 
+                                                        "nombre" : nombre, 
+                                                        "edad" : edad,
+                                                        "peso" : peso,
+                                                        "sexo" : sexo,
+                                                        "cuestion 1" : cuestion1,
+                                                        "cuestion 2" : cuestion1,
+                                                        "cuestion 3" : cuestion1,
+                                                        "cuestion 4" : cuestion1,
+                                                        "cuestion 5" : cuestion1,
+                                                        "cuestion 6" : cuestion1,
+                                                        "cuestion 7" : cuestion1,
+                                                        "cuestion 8" : cuestion1,
+                                                        "cuestion 9" : cuestion1,
+                                                        "cuestion 10" : cuestion1,
+                                                        "cuestion 11" : cuestion1,
+                                                        "cuestion 12" : cuestion1,
+                                                        "cuestion 13" : cuestion1,
+                                                        "cuestion 14" : cuestion1,
+                                                        "cuestion 15" : cuestion1,
+                                                        "cuestion 16" : cuestion1,})
    
