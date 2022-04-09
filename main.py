1f51570864e53fddf517319c77fcbfe1d9c62716
FranZajara <franzajaragomez@gmail.com>
2022-04-09 20:11:07 +0200

    algoritmo terminado

--- main.py
+++ main.py
@@ -33,16 +33,87 @@ async def analisis( request: Request,
                 cuestion15: int = Form(...),
                 cuestion16: int = Form(...)):
     
+    #guardamos el indice de masa corporal del paciente
+    imc = IMC(peso,altura)
+    
+    #guardamos el indice metabolico basal del paciente
+    imb = IMB(sexo,altura,peso,edad)
+    
+    #guardamos en una variable si el paciente presenta o no el trastorno de Pica.
+    if (Pica(cuestion1,cuestion2) == True):
+        pica = "El paciente probablemente presenta el trastorno alimentario de Pica."
+    else:
+        pica = "El paciente no presenta el trastorno alimentario de Pica."
+    
+    #guardamos en una variable si el paciente presenta o no el trastorno de Pica.
+    if(Rumiacion(cuestion3) == True):
+        rumiacion = "El paciente probablemente presenta el trastorno alimentario de Rumiación."
+    else:
+        rumiacion = "El paciente no presenta el trastorno alimentario de Rumiación."
+
+    #guardamos en una variable si el paciente presenta o no el trastorno de evitación.
+    if (Evitacion(cuestion4,cuestion5) == True):
+        evitacion = "El paciente probablemente presenta el trastorno alimentario de Evitación."
+    else:
+        evitacion = "El paciente no presenta el trastorno alimentario de Evitación."
+    
+    #guardamos en una variable si el paciente presenta o no el trastorno de Anorexia nerviosa.
+    if(Anorexia(cuestion4,cuestion6,cuestion7,cuestion8,cuestion9) == True):
+        anorexia = "El paciente probablemente presenta el trastorno alimentario de Anorexia nerviosa, "
+        if((cuestion8 == 1) | (cuestion9 == 1) | (cuestion10 == 1)):
+            tipoanorexia = "tipo con atracones o purgas, "
+        else:
+            tipoanorexia = "tipo restrictivo, "
+        
+        if(imc >= 17):
+            severidad = "severidad leve."
+        elif(16 <= imc < 17):
+            severidad = "severidad moderada."
+        elif(15 <= imc < 16):
+            severidad = "severidad grave."
+        else:
+            severidad = "severidad extrema."
+        anorexia = anorexia + tipoanorexia + severidad
+    else:
+        anorexia = "El paciente no presenta el trastorno alimentario de Anorexia nerviosa."
     
     
+    if(Bulimia(cuestion10,cuestion11,cuestion9,cuestion12,cuestion13,cuestion16) == True):
+        bulimia = "El paciente probablemente presenta el trastorno alimentario de Bulimia nerviosa, "
+        if(cuestion16 == 0):
+            severidad2 = "severidad leve."
+        elif(cuestion16 == 1):
+            severidad2 = "severidad moderada."
+        elif(cuestion16 == 2):
+            severidad2 = "severidad grave."
+        else:
+            severidad2 = "severidad extrema."
+        bulimia = bulimia + severidad2
+    else:
+        bulimia = "El paciente no presenta el trastorno alimentario de Bulimia nerviosa."
+
+    cuenta = cuestion14.len()
+
+    if(Atracon(cuestion10,cuestion11, cuenta, cuestion13) == True):
+        atracon = "El paciente probablemente presenta el trastorno alimentario de Atracones, "
+        if(cuestion13 == 0):
+            severidad3 = "severidad leve."
+        elif(cuestion13 == 1):
+            severidad3 = "severidad moderada."
+        elif(cuestion13 == 2):
+            severidad3 = "severidad grave."
+        else:
+            severidad3 = "severidad extrema."
+        atracon = atracon + severidad3
     
-    
-    
-    
-    
-    
-    
-    print(request)
+    else:
+        atracon = "El paciente no presenta el trastorno alimentario de Atracones."
+
+    if(Nocturno(cuestion15) == True):
+        nocturno = "El paciente probablemente presenta el síndrome de ingestión nocturna de alimentos."
+    else:
+        nocturno = "El paciente no presenta el síndrome de ingestión nocturna de alimentos."
+   
     return             templates.TemplateResponse ("analisis.html", {
                                                         "request" : request,
                                                         "nombre" : nombre, 
@@ -50,20 +121,14 @@ async def analisis( request: Request,
                                                         "peso" : peso,
                                                         "sexo" : sexo,
                                                         "altura" : altura,
-                                                        "cuestion1" : cuestion1,
-                                                        "cuestion2" : cuestion2,
-                                                        "cuestion3" : cuestion3,
-                                                        "cuestion4" : cuestion4,
-                                                        "cuestion5" : cuestion5,
-                                                        "cuestion6" : cuestion6,
-                                                        "cuestion7" : cuestion7,
-                                                        "cuestion8" : cuestion8,
-                                                        "cuestion9" : cuestion9,
-                                                        "cuestion10" : cuestion10,
-                                                        "cuestion11" : cuestion11,
-                                                        "cuestion12" : cuestion12,
-                                                        "cuestion13" : cuestion13,
-                                                        "cuestion14" : cuestion14,
-                                                        "cuestion15" : cuestion15,
-                                                        "cuestion16" : cuestion16})
+                                                        "imc" : imc,
+                                                        "imb" : imb,
+                                                        "pica" : pica,
+                                                        "rumiación" : rumiacion,
+                                                        "evitacion" : evitacion,
+                                                        "anorexia" : anorexia,
+                                                        "bulimia" : bulimia,
+                                                        "atracon" : atracon,
+                                                        "nocturno" : nocturno
+                                                        })
    
