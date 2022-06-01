40d4617cc34cdb9cdaa9689fb9864990c6ffca65
FranZajara <franzajaragomez@gmail.com>
2022-06-01 13:28:25 +0200

    arreglo

---
+++ funciones.py
@@ -0,0 +1,88 @@
+from sklearn import tree
+import pandas as pd
+import numpy
+
+#función para calcular el índice metabólico basal
+def IMB(sexo,altura,peso, edad):
+    if(sexo == "Hombre"):
+        return 10*peso + 6.25*altura - 5*edad + 5
+    else:
+        return 10*peso + 6.25*altura - 5*edad -161
+
+#función para calcular el índice de masa corporal
+def IMC(peso, altura):
+    return peso/((altura/100) * (altura/100))
+
+#función para identificar si el paciente posee el trastorno alimenticio de Pica
+def Pica(C1, C2):
+    if(((C1 == 1) & (C2 == 1)) | ((C1 == 1) & (C2 == 0))):
+        return True
+    elif((C1 == 0) & (C2 == 1)):
+        return False
+    else:
+        return False
+
+#función para identificar si el paciente posee el trastorno alimenticio de Rumiación
+def Rumiacion(C3):
+    if(C3 == 1):
+        return True
+    else:
+        return False
+
+#función para identificar si el paciente posee el trastorno alimenticio de Evitación
+def Evitacion(C4,C5):
+    if((C4 == 1) & (C5 == 1) | (C4 == 0) & (C5 == 1) ):
+        return True
+    else:
+        return False
+
+#función para identificar si el paciente posee el trastorno alimenticio de Anorexia Nerviosa
+def Anorexia(C4,C6,C7,C8,C9):
+    datos = pd.read_csv('datos_anorexia.csv', sep = ';' , header=0)
+    datos = pd.read_csv('datos_anorexia.csv', sep = ';' , header=0)
+    datos2  = datos
+    res = datos['R'].to_list()
+    del(datos2['R'])
+    pro = datos2.to_numpy().tolist()
+    datos = pd.read_csv('datos_anorexia.csv',header=0)
+    classifier = tree.DecisionTreeClassifier()
+    classifier.fit(pro,res)
+    return classifier.predict(numpy.array([[C4,C6,C7,C8,C9]]))
+
+#función para identificar si el paciente posee el trastorno alimenticio de Bulimia Nerviosa
+def Bulimia(C4,C6,C7,C8,C9,C10):
+    datos = pd.read_csv('datos_bulimia.csv', sep = ';' , header=0)
+    datos = pd.read_csv('datos_bulimia.csv', sep = ';' , header=0)
+    datos2  = datos
+    res = datos['R1'].to_list()
+    del(datos2['R1'])
+    pro = datos2.to_numpy().tolist()
+    datos = pd.read_csv('datos_bulimia.csv',header=0)
+    classifier = tree.DecisionTreeClassifier()
+    classifier.fit(pro,res)
+    return classifier.predict(numpy.array([[C4,C6,C7,C8,C9,C10]]))
+
+#función para identificar si el paciente posee el trastorno alimenticio de Atracón
+def Atracon(C4,C6,C7,C8):
+    datos = pd.read_csv('datos_atracon.csv', sep = ';' , header=0)
+    datos = pd.read_csv('datos_atracon.csv', sep = ';' , header=0)
+    datos2  = datos
+    res = datos['R1'].to_list()
+    del(datos2['R1'])
+    pro = datos2.to_numpy().tolist()
+    datos = pd.read_csv('datos_atracon.csv',header=0)
+    classifier = tree.DecisionTreeClassifier()
+    classifier.fit(pro,res)
+    return classifier.predict(numpy.array([[C4,C6,C7,C8]]))
+
+
+#función para identificar si el paciente posee el Síndrome de ingesta nocturna
+
+def Nocturno(C15):
+    if (C15 == 1):
+        return True
+    else:
+        return False
+
+
+
---
+++ main.py
@@ -0,0 +1,174 @@
+from cgitb import html
+from fastapi import FastAPI, Request, Form
+import json
+
+from urllib3 import HTTPResponse
+from funciones import *
+from fastapi.encoders import jsonable_encoder
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+import pdfkit
+from xhtml2pdf import pisa
+from io import BytesIO
+from jinja2 import Environment, PackageLoader
+
+
+
+app = FastAPI()
+
+templates = Jinja2Templates(directory="templates")
+
+@app.post("/analisis/", response_class=HTMLResponse)
+async def analisis( request: Request,
+                nombre: str = Form(...), 
+                edad: int = Form(...), 
+                peso: int = Form(...),
+                sexo: str = Form(...),
+                altura: int = Form(...),
+                cuestion1: int = Form(...),
+                cuestion2: int = Form(...),
+                cuestion3: int = Form(...),
+                cuestion4: int = Form(...),
+                cuestion5: int = Form(...),
+                cuestion6: int = Form(...),
+                cuestion7: int = Form(...),
+                cuestion8: int = Form(...),
+                cuestion9: int = Form(...),
+                cuestion10: int = Form(...),
+                cuestion11: int = Form(...),
+                cuestion12: int = Form(...),
+                cuestion13: int = Form(...),
+                cuestion14: list = Form(...),
+                cuestion15: int = Form(...),
+                cuestion16: int = Form(...)):
+    
+    #guardamos el indice de masa corporal del paciente
+    imc = IMC(peso,altura)
+    
+    #guardamos el indice metabolico basal del paciente
+    imb = IMB(sexo,altura,peso,edad)
+    
+    #guardamos en una variable si el paciente presenta o no el trastorno de Pica.
+    if (Pica(cuestion1,cuestion2) == True):
+        pica = "El paciente probablemente presenta el trastorno alimentario de Pica."
+    else:
+        pica = "El paciente no presenta el trastorno alimentario de Pica."
+    
+    #guardamos en una variable si el paciente presenta o no el trastorno de Pica.
+    if(Rumiacion(cuestion3) == True):
+        rumiacion = "El paciente probablemente presenta el trastorno alimentario de Rumiación."
+    else:
+        rumiacion = "El paciente no presenta el trastorno alimentario de Rumiación."
+
+    #guardamos en una variable si el paciente presenta o no el trastorno de evitación.
+    if (Evitacion(cuestion4,cuestion5) == True):
+        evitacion = "El paciente probablemente presenta el trastorno alimentario de Evitación."
+    else:
+        evitacion = "El paciente no presenta el trastorno alimentario de Evitación."
+    
+    #guardamos en una variable si el paciente presenta o no el trastorno de Anorexia nerviosa.
+    if(Anorexia(cuestion4,cuestion6,cuestion7,cuestion8,cuestion9) == True):
+        anorexia = "El paciente probablemente presenta el trastorno alimentario de Anorexia nerviosa, "
+        if((cuestion8 == 1) | (cuestion9 == 1) | (cuestion10 == 1)):
+            tipoanorexia = "tipo con atracones o purgas, "
+        else:
+            tipoanorexia = "tipo restrictivo, "
+        
+        if(imc >= 17):
+            severidad = "severidad leve."
+        elif(16 <= imc < 17):
+            severidad = "severidad moderada."
+        elif(15 <= imc < 16):
+            severidad = "severidad grave."
+        else:
+            severidad = "severidad extrema."
+        anorexia = anorexia + tipoanorexia + severidad
+    else:
+        anorexia = "El paciente no presenta el trastorno alimentario de Anorexia nerviosa."
+    
+    
+    if(Bulimia(cuestion10,cuestion11,cuestion9,cuestion12,cuestion13,cuestion16) == True):
+        bulimia = "El paciente probablemente presenta el trastorno alimentario de Bulimia nerviosa, "
+        if(cuestion16 == 0):
+            severidad2 = "severidad leve."
+        elif(cuestion16 == 1):
+            severidad2 = "severidad moderada."
+        elif(cuestion16 == 2):
+            severidad2 = "severidad grave."
+        else:
+            severidad2 = "severidad extrema."
+        bulimia = bulimia + severidad2
+    else:
+        bulimia = "El paciente no presenta el trastorno alimentario de Bulimia nerviosa."
+
+    cuenta = len(cuestion14)
+
+    if(Atracon(cuestion10,cuestion11, cuenta, cuestion13) == True):
+        atracon = "El paciente probablemente presenta el trastorno alimentario de Atracones, "
+        if(cuestion13 == 0):
+            severidad3 = "severidad leve."
+        elif(cuestion13 == 1):
+            severidad3 = "severidad moderada."
+        elif(cuestion13 == 2):
+            severidad3 = "severidad grave."
+        else:
+            severidad3 = "severidad extrema."
+        atracon = atracon + severidad3
+    
+    else:
+        atracon = "El paciente no presenta el trastorno alimentario de Atracones."
+
+    if(Nocturno(cuestion15) == True):
+        nocturno = "El paciente probablemente presenta el síndrome de ingestión nocturna de alimentos."
+    else:
+        nocturno = "El paciente no presenta el síndrome de ingestión nocturna de alimentos."
+   
+    
+    dic = {
+                                                        "request" : request,
+                                                        "nombre" : nombre, 
+                                                        "edad" : edad,
+                                                        "peso" : peso,
+                                                        "sexo" : sexo,
+                                                        "altura" : altura,
+                                                        "imc" : imc,
+                                                        "imb" : imb,
+                                                        "pica" : pica,
+                                                        "rumiacion" : rumiacion,
+                                                        "evitacion" : evitacion,
+                                                        "anorexia" : anorexia,
+                                                        "bulimia" : bulimia,
+                                                        "atracon" : atracon,
+                                                        "nocturno" : nocturno
+                                                        }
+
+    def render_to_pdf(template_src, context_dict={}):
+        env = Environment(loader=PackageLoader('FASTAPI-EATSAFE', 'templates'))
+        template = env.get_template(template_src)
+        html = template.render(context_dict)
+        result = BytesIO()
+        pdf = pisa.pisaDocument(BytesIO(html.encode("ISO-8859-1")), result)
+        if not pdf.err:
+            return HTTPResponse(result.getvalue())
+        return None
+  
+    return render_to_pdf("analisis.html", dic)
+    ##ufbdufbi
+   ## return             templates.TemplateResponse ("analisis.html", {
+   ##                                                     "request" : request,
+    ##                                                    "nombre" : nombre, 
+    ##                                                    "edad" : edad,
+    ##                                                    "peso" : peso,
+    ##                                                    "sexo" : sexo,
+    ##                                                    "altura" : altura,
+     ##                                                   "imc" : imc,
+     ##                                                   "imb" : imb,
+    ##                                                    "pica" : pica,
+    ##                                                    "rumiacion" : rumiacion,
+    ##                                                    "evitacion" : evitacion,
+    ##                                                    "anorexia" : anorexia,
+    ##                                                    "bulimia" : bulimia,
+     ##                                                   "atracon" : atracon,
+     ##                                                   "nocturno" : nocturno
+     ##                                                   })
+   
